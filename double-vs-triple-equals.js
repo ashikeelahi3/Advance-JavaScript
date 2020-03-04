ecc8fb7466fcc74e8a7c84a5247cc139d3c867d6
ashikeelahi3 <ashikeelahi3@gmail.com>
2020-03-05 04:49:56 +0600

    adding 3 file

---
+++ double-vs-triple-equals.js
@@ -0,0 +1,26 @@
+const first = 2;
+const second = '2';
+
+if(first == second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+// Here condition is true
+
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+// Here condition is false. Because, type is not equal
+
+1 == true;
+0 == false;
+//these conditions are true
+
+1 === true;
+0 === false;
+//these conditions are false
