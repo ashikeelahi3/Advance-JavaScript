9128b96e87280ce73589611625a23a083b57e50f
ashikeelahi3 <ashikeelahi3@gmail.com>
2020-03-05 03:30:51 +0600

    true vs false

---
+++ true-fals.js
@@ -0,0 +1,40 @@
+//False:
+// 0
+// ""
+// ''
+// undefined
+// null
+// NaN
+// false
+
+//Truthy
+//'0'
+//" "
+// ' '
+// {}
+// []
+// 'false'
+
+
+
+const name = 0
+
+if (name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+///////////////////////////////////////////
+// const name = 0
+
+// if (name || name == 0){              
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+//....eikhetre condition is true
