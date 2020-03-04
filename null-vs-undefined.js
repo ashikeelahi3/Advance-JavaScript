34df5f7e1328f7aab7260375cc94f85f8c8941be
ashikeelahi3 <ashikeelahi3@gmail.com>
2020-03-05 04:00:29 +0600

    hi

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+Undefined 
+
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+const lover = {name:"Ashik", phone:01406;}
+console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
+
+
